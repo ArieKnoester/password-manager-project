135a0db585db020ec2b389bbb9a6d305116d1096
Arie Knoester <arikstr@gmail.com>
2023-10-17 10:59:56 -0500

    Added save to file functionality to the 'Add' button.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from tkinter import *
+DEFAULT_USERNAME = ""
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_password():
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", mode="a") as file:
+        file.write(f"{website} | {username} | {password}\n")
+
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+    website_entry.focus()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -25,7 +38,9 @@ password_label.grid(row=3, column=0, pady=5)
 # Entries
 website_entry = Entry()
 website_entry.grid(row=1, column=1, padx=(5, 0), pady=5, columnspan=2, sticky="EW")
+website_entry.focus()
 username_entry = Entry()
+username_entry.insert(INSERT, DEFAULT_USERNAME)
 username_entry.grid(row=2, column=1, padx=(5, 0), pady=5, columnspan=2, sticky="EW")
 password_entry = Entry(width=31)
 password_entry.grid(row=3, column=1, padx=(5, 0), pady=5, sticky="W")
@@ -33,7 +48,7 @@ password_entry.grid(row=3, column=1, padx=(5, 0), pady=5, sticky="W")
 # Buttons
 generate_button = Button(text="Generate Password")
 generate_button.grid(row=3, column=2, sticky="EW")
-add_button = Button(text="Add")
+add_button = Button(text="Add", command=add_password)
 add_button.grid(row=4, column=1, padx=(5, 0), pady=(5, 0), columnspan=2, sticky="EW")
 
 window.mainloop()
